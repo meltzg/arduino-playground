f8622ff6251cbe24d69f5d5803a9fba8c6127751
Greg Meltzer <vadercows@gmail.com>
2021-02-28 18:03:48 -0500

    get things working with common anode and 16 seg

--- Components/Components.cpp
+++ Components/Components.cpp
@@ -1,396 +1,603 @@
 #include <Arduino.h>
 #include <Components.h>
 
+#define __SEG_16__
+#define __COMMON_ANODE__
+
+#ifdef __COMMON_ANODE__
+#define SEG_ON HIGH
+#define SEG_OFF LOW
+#else
+#define SEG_ON LOW
+#define SEG_OFF HIGH
+#endif
+
 const __int24 RAINBOW[6] = {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE};
 
 const uint16_t getCharacter(char character)
 {
+  uint16_t charCode = 0;
   switch (character)
   {
 #ifdef __SEG_16__
   case '!':
-    return 0x000C; /* ! */
+    charCode = 0x000C; /* ! */
+    break;
   case '"':
-    return 0x0204; /* " */
+    charCode = 0x0204; /* " */
+    break;
   case '#':
-    return 0xAA3C; /* # */
+    charCode = 0xAA3C; /* # */
+    break;
   case '$':
-    return 0xAABB; /* $ */
+    charCode = 0xAABB; /* $ */
+    break;
   case '%':
-    return 0xEE99; /* % */
+    charCode = 0xEE99; /* % */
+    break;
   case '&':
-    return 0x9371; /* & */
+    charCode = 0x9371; /* & */
+    break;
   case '\'':
-    return 0x0200; /* ' */
+    charCode = 0x0200; /* ' */
+    break;
   case '(':
-    return 0x1400; /* ( */
+    charCode = 0x1400; /* ( */
+    break;
   case ')':
-    return 0x4100; /* ) */
+    charCode = 0x4100; /* ) */
+    break;
   case '*':
-    return 0xFF00; /* * */
+    charCode = 0xFF00; /* * */
+    break;
   case '+':
-    return 0xAA00; /* + */
+    charCode = 0xAA00; /* + */
+    break;
   case ',':
-    return 0x4000; /* ; */
+    charCode = 0x4000; /* ; */
+    break;
   case '-':
-    return 0x8800; /* - */
+    charCode = 0x8800; /* - */
+    break;
   case '.':
-    return 0x1000; /* . */
+    charCode = 0x1000; /* . */
+    break;
   case '/':
-    return 0x4400; /* / */
+    charCode = 0x4400; /* / */
+    break;
   case '0':
-    return 0x44FF; /* 0 */
+    charCode = 0x44FF; /* 0 */
+    break;
   case '1':
-    return 0x040C; /* 1 */
+    charCode = 0x040C; /* 1 */
+    break;
   case '2':
-    return 0x8877; /* 2 */
+    charCode = 0x8877; /* 2 */
+    break;
   case '3':
-    return 0x083F; /* 3 */
+    charCode = 0x083F; /* 3 */
+    break;
   case '4':
-    return 0x888C; /* 4 */
+    charCode = 0x888C; /* 4 */
+    break;
   case '5':
-    return 0x90B3; /* 5 */
+    charCode = 0x90B3; /* 5 */
+    break;
   case '6':
-    return 0x88FB; /* 6 */
+    charCode = 0x88FB; /* 6 */
+    break;
   case '7':
-    return 0x000F; /* 7 */
+    charCode = 0x000F; /* 7 */
+    break;
   case '8':
-    return 0x88FF; /* 8 */
+    charCode = 0x88FF; /* 8 */
+    break;
   case '9':
-    return 0x88BF; /* 9 */
+    charCode = 0x88BF; /* 9 */
+    break;
   case ':':
-    return 0x2200; /* : */
+    charCode = 0x2200; /* : */
+    break;
   case ';':
-    return 0x4200; /* ; */
+    charCode = 0x4200; /* ; */
+    break;
   case '<':
-    return 0x9400; /* < */
+    charCode = 0x9400; /* < */
+    break;
   case '=':
-    return 0x8830; /* = */
+    charCode = 0x8830; /* = */
+    break;
   case '>':
-    return 0x4900; /* > */
+    charCode = 0x4900; /* > */
+    break;
   case '?':
-    return 0x2807; /* ? */
+    charCode = 0x2807; /* ? */
+    break;
   case '@':
-    return 0x0AF7; /* @ */
+    charCode = 0x0AF7; /* @ */
+    break;
   case 'A':
-    return 0x88CF; /* A */
+    charCode = 0x88CF; /* A */
+    break;
   case 'B':
-    return 0x2A3F; /* B */
+    charCode = 0x2A3F; /* B */
+    break;
   case 'C':
-    return 0x00F3; /* C */
+    charCode = 0x00F3; /* C */
+    break;
   case 'D':
-    return 0x223F; /* D */
+    charCode = 0x223F; /* D */
+    break;
   case 'E':
-    return 0x80F3; /* E */
+    charCode = 0x80F3; /* E */
+    break;
   case 'F':
-    return 0x80C3; /* F */
+    charCode = 0x80C3; /* F */
+    break;
   case 'G':
-    return 0x08FB; /* G */
+    charCode = 0x08FB; /* G */
+    break;
   case 'H':
-    return 0x88CC; /* H */
+    charCode = 0x88CC; /* H */
+    break;
   case 'I':
-    return 0x2233; /* I */
+    charCode = 0x2233; /* I */
+    break;
   case 'J':
-    return 0x007C; /* J */
+    charCode = 0x007C; /* J */
+    break;
   case 'K':
-    return 0x94C0; /* K */
+    charCode = 0x94C0; /* K */
+    break;
   case 'L':
-    return 0x00F0; /* L */
+    charCode = 0x00F0; /* L */
+    break;
   case 'M':
-    return 0x05CC; /* M */
+    charCode = 0x05CC; /* M */
+    break;
   case 'N':
-    return 0x11CC; /* N */
+    charCode = 0x11CC; /* N */
+    break;
   case 'O':
-    return 0x00FF; /* O */
+    charCode = 0x00FF; /* O */
+    break;
   case 'P':
-    return 0x88C7; /* P */
+    charCode = 0x88C7; /* P */
+    break;
   case 'Q':
-    return 0x10FF; /* Q */
+    charCode = 0x10FF; /* Q */
+    break;
   case 'R':
-    return 0x98C7; /* R */
+    charCode = 0x98C7; /* R */
+    break;
   case 'S':
-    return 0x88BB; /* S */
+    charCode = 0x88BB; /* S */
+    break;
   case 'T':
-    return 0x2203; /* T */
+    charCode = 0x2203; /* T */
+    break;
   case 'U':
-    return 0x00FC; /* U */
+    charCode = 0x00FC; /* U */
+    break;
   case 'V':
-    return 0x44C0; /* V */
+    charCode = 0x44C0; /* V */
+    break;
   case 'W':
-    return 0x50CC; /* W */
+    charCode = 0x50CC; /* W */
+    break;
   case 'X':
-    return 0x5500; /* X */
+    charCode = 0x5500; /* X */
+    break;
   case 'Y':
-    return 0x88BC; /* Y */
+    charCode = 0x88BC; /* Y */
+    break;
   case 'Z':
-    return 0x4433; /* Z */
+    charCode = 0x4433; /* Z */
+    break;
   case '[':
-    return 0x2212; /* [ */
+    charCode = 0x2212; /* [ */
+    break;
   case ']':
-    return 0x1100; /* \ */
+    charCode = 0x1100; /* \ */
+    break;
   case '\\':
-    return 0x2221; /* ] */
+    charCode = 0x2221; /* ] */
+    break;
   case '^':
-    return 0x5000; /* ^ */
+    charCode = 0x5000; /* ^ */
+    break;
   case '_':
-    return 0x0030; /* _ */
+    charCode = 0x0030; /* _ */
+    break;
   case '`':
-    return 0x0100; /* ` */
+    charCode = 0x0100; /* ` */
+    break;
   case 'a':
-    return 0xA070; /* a */
+    charCode = 0xA070; /* a */
+    break;
   case 'b':
-    return 0xA0E0; /* b */
+    charCode = 0xA0E0; /* b */
+    break;
   case 'c':
-    return 0x8060; /* c */
+    charCode = 0x8060; /* c */
+    break;
   case 'd':
-    return 0x281C; /* d */
+    charCode = 0x281C; /* d */
+    break;
   case 'e':
-    return 0xC060; /* e */
+    charCode = 0xC060; /* e */
+    break;
   case 'f':
-    return 0xAA02; /* f */
+    charCode = 0xAA02; /* f */
+    break;
   case 'g':
-    return 0xA2A1; /* g */
+    charCode = 0xA2A1; /* g */
+    break;
   case 'h':
-    return 0xA0C0; /* h */
+    charCode = 0xA0C0; /* h */
+    break;
   case 'i':
-    return 0x2000; /* i */
+    charCode = 0x2000; /* i */
+    break;
   case 'j':
-    return 0x2260; /* j */
+    charCode = 0x2260; /* j */
+    break;
   case 'k':
-    return 0x3600; /* k */
+    charCode = 0x3600; /* k */
+    break;
   case 'l':
-    return 0x00C0; /* l */
+    charCode = 0x00C0; /* l */
+    break;
   case 'm':
-    return 0xA848; /* m */
+    charCode = 0xA848; /* m */
+    break;
   case 'n':
-    return 0xA040; /* n */
+    charCode = 0xA040; /* n */
+    break;
   case 'o':
-    return 0xA060; /* o */
+    charCode = 0xA060; /* o */
+    break;
   case 'p':
-    return 0x82C1; /* p */
+    charCode = 0x82C1; /* p */
+    break;
   case 'q':
-    return 0xA281; /* q */
+    charCode = 0xA281; /* q */
+    break;
   case 'r':
-    return 0x8040; /* r */
+    charCode = 0x8040; /* r */
+    break;
   case 's':
-    return 0xA0A1; /* s */
+    charCode = 0xA0A1; /* s */
+    break;
   case 't':
-    return 0x80E0; /* t */
+    charCode = 0x80E0; /* t */
+    break;
   case 'u':
-    return 0x2060; /* u */
+    charCode = 0x2060; /* u */
+    break;
   case 'v':
-    return 0x4040; /* v */
+    charCode = 0x4040; /* v */
+    break;
   case 'w':
-    return 0x5048; /* w */
+    charCode = 0x5048; /* w */
+    break;
   case 'x':
-    return 0x5500; /* x */
+    charCode = 0x5500; /* x */
+    break;
   case 'y':
-    return 0x0A1C; /* y */
+    charCode = 0x0A1C; /* y */
+    break;
   case 'z':
-    return 0xC020; /* z */
+    charCode = 0xC020; /* z */
+    break;
   case '{':
-    return 0xA212; /* { */
+    charCode = 0xA212; /* { */
+    break;
   case '|':
-    return 0x2200; /* | */
+    charCode = 0x2200; /* | */
+    break;
   case '}':
-    return 0x2A21; /* } */
+    charCode = 0x2A21; /* } */
+    break;
   case '~':
-    return 0xCC00; /* ~ */
+    charCode = 0xCC00; /* ~ */
+    break;
   default:
-    return 0x0000; /* space */
+    charCode = 0x0000; /* space */
+    break;
 #else
   case '!':
-    return 0x4006; /* ! */
+    charCode = 0x4006; /* ! */
+    break;
   case '"':
-    return 0x0202; /* " */
+    charCode = 0x0202; /* " */
+    break;
   case '#':
-    return 0x12CE; /* # */
+    charCode = 0x12CE; /* # */
+    break;
   case '$':
-    return 0x12ED; /* $ */
+    charCode = 0x12ED; /* $ */
+    break;
   case '%':
-    return 0x3FE4; /* % */
+    charCode = 0x3FE4; /* % */
+    break;
   case '&':
-    return 0x2359; /* & */
+    charCode = 0x2359; /* & */
+    break;
   case '\'':
-    return 0x0200; /* ' */
+    charCode = 0x0200; /* ' */
+    break;
   case '(':
-    return 0x2400; /* ( */
+    charCode = 0x2400; /* ( */
+    break;
   case ')':
-    return 0x0900; /* ) */
+    charCode = 0x0900; /* ) */
+    break;
   case '*':
-    return 0x3FC0; /* * */
+    charCode = 0x3FC0; /* * */
+    break;
   case '+':
-    return 0x12C0; /* + */
+    charCode = 0x12C0; /* + */
+    break;
   case ',':
-    return 0x0800; /* , */
+    charCode = 0x0800; /* , */
+    break;
   case '-':
-    return 0x00C0; /* - */
+    charCode = 0x00C0; /* - */
+    break;
   case '.':
-    return 0x4000; /* . */
+    charCode = 0x4000; /* . */
+    break;
   case '/':
-    return 0x0C00; /* / */
+    charCode = 0x0C00; /* / */
+    break;
   case '0':
-    return 0x0C3F; /* 0 */
+    charCode = 0x0C3F; /* 0 */
+    break;
   case '1':
-    return 0x0406; /* 1 */
+    charCode = 0x0406; /* 1 */
+    break;
   case '2':
-    return 0x00DB; /* 2 */
+    charCode = 0x00DB; /* 2 */
+    break;
   case '3':
-    return 0x008F; /* 3 */
+    charCode = 0x008F; /* 3 */
+    break;
   case '4':
-    return 0x00E6; /* 4 */
+    charCode = 0x00E6; /* 4 */
+    break;
   case '5':
-    return 0x2069; /* 5 */
+    charCode = 0x2069; /* 5 */
+    break;
   case '6':
-    return 0x00FD; /* 6 */
+    charCode = 0x00FD; /* 6 */
+    break;
   case '7':
-    return 0x0007; /* 7 */
+    charCode = 0x0007; /* 7 */
+    break;
   case '8':
-    return 0x00FF; /* 8 */
+    charCode = 0x00FF; /* 8 */
+    break;
   case '9':
-    return 0x00EF; /* 9 */
+    charCode = 0x00EF; /* 9 */
+    break;
   case ':':
-    return 0x1200; /* : */
+    charCode = 0x1200; /* : */
+    break;
   case ';':
-    return 0x0A00; /* ; */
+    charCode = 0x0A00; /* ; */
+    break;
   case '<':
-    return 0x2440; /* < */
+    charCode = 0x2440; /* < */
+    break;
   case '=':
-    return 0x00C8; /* = */
+    charCode = 0x00C8; /* = */
+    break;
   case '>':
-    return 0x0980; /* > */
+    charCode = 0x0980; /* > */
+    break;
   case '?':
-    return 0x5083; /* ? */
+    charCode = 0x5083; /* ? */
+    break;
   case '@':
-    return 0x02BB; /* @ */
+    charCode = 0x02BB; /* @ */
+    break;
   case 'A':
-    return 0x00F7; /* A */
+    charCode = 0x00F7; /* A */
+    break;
   case 'B':
-    return 0x128F; /* B */
+    charCode = 0x128F; /* B */
+    break;
   case 'C':
-    return 0x0039; /* C */
+    charCode = 0x0039; /* C */
+    break;
   case 'D':
-    return 0x120F; /* D */
+    charCode = 0x120F; /* D */
+    break;
   case 'E':
-    return 0x0079; /* E */
+    charCode = 0x0079; /* E */
+    break;
   case 'F':
-    return 0x0071; /* F */
+    charCode = 0x0071; /* F */
+    break;
   case 'G':
-    return 0x00BD; /* G */
+    charCode = 0x00BD; /* G */
+    break;
   case 'H':
-    return 0x00F6; /* H */
+    charCode = 0x00F6; /* H */
+    break;
   case 'I':
-    return 0x1209; /* I */
+    charCode = 0x1209; /* I */
+    break;
   case 'J':
-    return 0x001E; /* J */
+    charCode = 0x001E; /* J */
+    break;
   case 'K':
-    return 0x2470; /* K */
+    charCode = 0x2470; /* K */
+    break;
   case 'L':
-    return 0x0038; /* L */
+    charCode = 0x0038; /* L */
+    break;
   case 'M':
-    return 0x0536; /* M */
+    charCode = 0x0536; /* M */
+    break;
   case 'N':
-    return 0x2136; /* N */
+    charCode = 0x2136; /* N */
+    break;
   case 'O':
-    return 0x003F; /* O */
+    charCode = 0x003F; /* O */
+    break;
   case 'P':
-    return 0x00F3; /* P */
+    charCode = 0x00F3; /* P */
+    break;
   case 'Q':
-    return 0x203F; /* Q */
+    charCode = 0x203F; /* Q */
+    break;
   case 'R':
-    return 0x20F3; /* R */
+    charCode = 0x20F3; /* R */
+    break;
   case 'S':
-    return 0x00ED; /* S */
+    charCode = 0x00ED; /* S */
+    break;
   case 'T':
-    return 0x1201; /* T */
+    charCode = 0x1201; /* T */
+    break;
   case 'U':
-    return 0x003E; /* U */
+    charCode = 0x003E; /* U */
+    break;
   case 'V':
-    return 0x0C30; /* V */
+    charCode = 0x0C30; /* V */
+    break;
   case 'W':
-    return 0x2836; /* W */
+    charCode = 0x2836; /* W */
+    break;
   case 'X':
-    return 0x2D00; /* X */
+    charCode = 0x2D00; /* X */
+    break;
   case 'Y':
-    return 0x00EE; /* Y */
+    charCode = 0x00EE; /* Y */
+    break;
   case 'Z':
-    return 0x0C09; /* Z */
+    charCode = 0x0C09; /* Z */
+    break;
   case '[':
-    return 0x0039; /* [ */
+    charCode = 0x0039; /* [ */
+    break;
   case '\\':
-    return 0x2100; /* \ */
+    charCode = 0x2100; /* \ */
+    break;
   case ']':
-    return 0x000F; /* ] */
+    charCode = 0x000F; /* ] */
+    break;
   case '^':
-    return 0x2800; /* ^ */
+    charCode = 0x2800; /* ^ */
+    break;
   case '_':
-    return 0x0008; /* _ */
+    charCode = 0x0008; /* _ */
+    break;
   case '`':
-    return 0x0100; /* ` */
+    charCode = 0x0100; /* ` */
+    break;
   case 'a':
-    return 0x1058; /* a */
+    charCode = 0x1058; /* a */
+    break;
   case 'b':
-    return 0x2078; /* b */
+    charCode = 0x2078; /* b */
+    break;
   case 'c':
-    return 0x00D8; /* c */
+    charCode = 0x00D8; /* c */
+    break;
   case 'd':
-    return 0x088E; /* d */
+    charCode = 0x088E; /* d */
+    break;
   case 'e':
-    return 0x0858; /* e */
+    charCode = 0x0858; /* e */
+    break;
   case 'f':
-    return 0x14C0; /* f */
+    charCode = 0x14C0; /* f */
+    break;
   case 'g':
-    return 0x048E; /* g */
+    charCode = 0x048E; /* g */
+    break;
   case 'h':
-    return 0x1070; /* h */
+    charCode = 0x1070; /* h */
+    break;
   case 'i':
-    return 0x1000; /* i */
+    charCode = 0x1000; /* i */
+    break;
   case 'j':
-    return 0x0A10; /* j */
+    charCode = 0x0A10; /* j */
+    break;
   case 'k':
-    return 0x3600; /* k */
+    charCode = 0x3600; /* k */
+    break;
   case 'l':
-    return 0x0030; /* l */
+    charCode = 0x0030; /* l */
+    break;
   case 'm':
-    return 0x10D4; /* m */
+    charCode = 0x10D4; /* m */
+    break;
   case 'n':
-    return 0x1050; /* n */
+    charCode = 0x1050; /* n */
+    break;
   case 'o':
-    return 0x00DC; /* o */
+    charCode = 0x00DC; /* o */
+    break;
   case 'p':
-    return 0x0170; /* p */
+    charCode = 0x0170; /* p */
+    break;
   case 'q':
-    return 0x0486; /* q */
+    charCode = 0x0486; /* q */
+    break;
   case 'r':
-    return 0x0050; /* r */
+    charCode = 0x0050; /* r */
+    break;
   case 's':
-    return 0x2088; /* s */
+    charCode = 0x2088; /* s */
+    break;
   case 't':
-    return 0x0078; /* t */
+    charCode = 0x0078; /* t */
+    break;
   case 'u':
-    return 0x001C; /* u */
+    charCode = 0x001C; /* u */
+    break;
   case 'v':
-    return 0x0810; /* v */
+    charCode = 0x0810; /* v */
+    break;
   case 'w':
-    return 0x2814; /* w */
+    charCode = 0x2814; /* w */
+    break;
   case 'x':
-    return 0x2D00; /* x */
+    charCode = 0x2D00; /* x */
+    break;
   case 'y':
-    return 0x028E; /* y */
+    charCode = 0x028E; /* y */
+    break;
   case 'z':
-    return 0x0848; /* z */
+    charCode = 0x0848; /* z */
+    break;
   case '{':
-    return 0x0949; /* { */
+    charCode = 0x0949; /* { */
+    break;
   case '|':
-    return 0x1200; /* | */
+    charCode = 0x1200; /* | */
+    break;
   case '}':
-    return 0x2489; /* } */
+    charCode = 0x2489; /* } */
+    break;
   case '~':
-    return 0x0CC0; /* ~ */
+    charCode = 0x0CC0; /* ~ */
+    break;
   default:
-    return 0x0000; /* (space) */
+    charCode = 0x0000; /* (space) */
+    break;
 #endif
   }
+
+#ifdef __COMMON_ANODE__
+  charCode = ~charCode;
+#endif
+  return charCode;
 }
 
 SegmentDisplay::SegmentDisplay(int latchPin, int clockPin, int dataPin, int leftCommonPin, int rightCommonPin) : latchPin(latchPin), clockPin(clockPin), dataPin(dataPin), leftCommonPin(leftCommonPin), rightCommonPin(rightCommonPin)
@@ -431,15 +638,15 @@ void SegmentDisplay::render(unsigned long currentMillis)
     // Assume common anode. to turn just left on, right must be high, left must be low
     if (showLeft)
     {
-      digitalWrite(rightCommonPin, HIGH);
+      digitalWrite(rightCommonPin, SEG_OFF);
       registerWrite(getCharacter(chars[0]));
-      digitalWrite(leftCommonPin, LOW);
+      digitalWrite(leftCommonPin, SEG_ON);
     }
     else
     {
-      digitalWrite(leftCommonPin, HIGH);
+      digitalWrite(leftCommonPin, SEG_OFF);
       registerWrite(getCharacter(chars[1]));
-      digitalWrite(rightCommonPin, LOW);
+      digitalWrite(rightCommonPin, SEG_ON);
     }
   }
 }

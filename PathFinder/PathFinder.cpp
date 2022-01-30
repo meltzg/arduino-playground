4510198bfb4c58886d38e6cd6b4223766a275815
Greg Meltzer <vadercows@gmail.com>
2022-01-30 02:43:55 -0500

    try using new timeout stuff for wire

--- PathFinder/PathFinder.cpp
+++ PathFinder/PathFinder.cpp
@@ -4,14 +4,40 @@
 
 void PathFinder::addNode(NodeId_t node, NodeId_t *neighbors, size_t numNeighbors)
 {
-    Wire.beginTransmission(FINDER_I2C_ADDR);
-    Wire.write(FINDER_ADD_NODE);
-    Wire.write((byte *)&node, sizeof(NodeId_t));
-    Wire.write((byte *)&numNeighbors, sizeof(size_t));
-    Wire.write((byte *)neighbors, sizeof(NodeId_t) * numNeighbors);
-    Wire.endTransmission();
+    int retries = 0;
+    do
+    {
+        do
+        {
+            retries++;
+            Serial.print(F("trn add node "));
+            Serial.println(retries);
+            Wire.clearWireTimeoutFlag();
+            Wire.beginTransmission(FINDER_I2C_ADDR);
+            Wire.write(FINDER_ADD_NODE);
+            Wire.write((byte *)&node, sizeof(NodeId_t));
+            Wire.write((byte *)&numNeighbors, sizeof(size_t));
+            Wire.write((byte *)neighbors, sizeof(NodeId_t) * numNeighbors);
+            Wire.endTransmission();
+            if (Wire.getWireTimeoutFlag())
+            {
+                delay(1000);
+            }
+        } while (Wire.getWireTimeoutFlag());
 
-    Wire.requestFrom(FINDER_I2C_ADDR, 1);
+        retries = 0;
+
+        retries++;
+        Serial.print(F("req add node "));
+        Serial.println(retries);
+        Wire.clearWireTimeoutFlag();
+        Wire.requestFrom(FINDER_I2C_ADDR, 1);
+        if (Wire.getWireTimeoutFlag())
+        {
+            delay(1000);
+        }
+    } while (Wire.getWireTimeoutFlag());
+
     while (Wire.available() < 1)
     {
     }
@@ -19,13 +45,39 @@ void PathFinder::addNode(NodeId_t node, NodeId_t *neighbors, size_t numNeighbors
 
 NodeId_t PathFinder::getNextStep(NodeId_t src, NodeId_t dest)
 {
-    Wire.beginTransmission(FINDER_I2C_ADDR);
-    Wire.write(FINDER_GET_NEXT_STEP);
-    Wire.write((byte *)&src, sizeof(NodeId_t));
-    Wire.write((byte *)&dest, sizeof(NodeId_t));
-    Wire.endTransmission();
+    int retries = 0;
+    do
+    {
+        do
+        {
+            retries++;
+            Serial.print(F("trn next step "));
+            Serial.println(retries);
+            Wire.clearWireTimeoutFlag();
+            Wire.beginTransmission(FINDER_I2C_ADDR);
+            Wire.write(FINDER_GET_NEXT_STEP);
+            Wire.write((byte *)&src, sizeof(NodeId_t));
+            Wire.write((byte *)&dest, sizeof(NodeId_t));
+            Wire.endTransmission();
+            if (Wire.getWireTimeoutFlag())
+            {
+                delay(1000);
+            }
+        } while (Wire.getWireTimeoutFlag());
 
-    Wire.requestFrom(FINDER_I2C_ADDR, sizeof(NodeId_t));
+        retries = 0;
+
+        retries++;
+        Serial.print(F("req next step "));
+        Serial.println(retries);
+        Wire.clearWireTimeoutFlag();
+        Wire.requestFrom(FINDER_I2C_ADDR, sizeof(NodeId_t));
+        if (Wire.getWireTimeoutFlag())
+        {
+            delay(1000);
+        }
+    } while (Wire.getWireTimeoutFlag());
+
     while (Wire.available() < sizeof(NodeId_t))
     {
     }
@@ -36,11 +88,37 @@ NodeId_t PathFinder::getNextStep(NodeId_t src, NodeId_t dest)
 
 void PathFinder::startDiscovery()
 {
-    Wire.beginTransmission(FINDER_I2C_ADDR);
-    Wire.write(FINDER_START_DISCOVERY);
-    Wire.endTransmission();
+    int retries = 0;
+    do
+    {
+        do
+        {
+            retries++;
+            Serial.print(F("trn start disco "));
+            Serial.println(retries);
+            Wire.clearWireTimeoutFlag();
+            Wire.beginTransmission(FINDER_I2C_ADDR);
+            Wire.write(FINDER_START_DISCOVERY);
+            Wire.endTransmission();
+            if (Wire.getWireTimeoutFlag())
+            {
+                delay(1000);
+            }
+        } while (Wire.getWireTimeoutFlag());
 
-    Wire.requestFrom(FINDER_I2C_ADDR, 1);
+        retries = 0;
+
+        retries++;
+        Serial.print(F("req start disco "));
+        Serial.println(retries);
+        Wire.clearWireTimeoutFlag();
+        Wire.requestFrom(FINDER_I2C_ADDR, 1);
+        if (Wire.getWireTimeoutFlag())
+        {
+            delay(1000);
+        }
+    } while (Wire.getWireTimeoutFlag());
+
     while (Wire.available() < 1)
     {
     }
@@ -48,12 +126,38 @@ void PathFinder::startDiscovery()
 
 DiscoveryStats PathFinder::getDiscoveryStats()
 {
-    Wire.beginTransmission(FINDER_I2C_ADDR);
-    Wire.write(FINDER_GET_DISCOVERY_STATS);
-    Wire.endTransmission();
-
     size_t statSize = sizeof(bool) + sizeof(size_t) * 2;
-    Wire.requestFrom(FINDER_I2C_ADDR, statSize);
+    int retries = 0;
+    do
+    {
+        do
+        {
+            retries++;
+            Serial.print(F("trn disco stats "));
+            Serial.println(retries);
+            Wire.clearWireTimeoutFlag();
+            Wire.beginTransmission(FINDER_I2C_ADDR);
+            Wire.write(FINDER_GET_DISCOVERY_STATS);
+            Wire.endTransmission();
+            if (Wire.getWireTimeoutFlag())
+            {
+                delay(1000);
+            }
+        } while (Wire.getWireTimeoutFlag());
+
+        retries = 0;
+
+        retries++;
+        Serial.print(F("req disco stats "));
+        Serial.println(retries);
+        Wire.clearWireTimeoutFlag();
+        Wire.requestFrom(FINDER_I2C_ADDR, statSize);
+        if (Wire.getWireTimeoutFlag())
+        {
+            delay(1000);
+        }
+    } while (Wire.getWireTimeoutFlag());
+
     while (Wire.available() < statSize)
     {
     }
@@ -70,11 +174,37 @@ DiscoveryStats PathFinder::getDiscoveryStats()
 
 NodeId_t PathFinder::getNextNeighborRequest()
 {
-    Wire.beginTransmission(FINDER_I2C_ADDR);
-    Wire.write(FINDER_GET_NEIGHBOR_REQUEST);
-    Wire.endTransmission();
+    int retries = 0;
+    do
+    {
+        do
+        {
+            retries++;
+            Serial.print(F("trn nxt neigh "));
+            Serial.println(retries);
+            Wire.clearWireTimeoutFlag();
+            Wire.beginTransmission(FINDER_I2C_ADDR);
+            Wire.write(FINDER_GET_NEIGHBOR_REQUEST);
+            Wire.endTransmission();
+            if (Wire.getWireTimeoutFlag())
+            {
+                delay(1000);
+            }
+        } while (Wire.getWireTimeoutFlag());
 
-    Wire.requestFrom(FINDER_I2C_ADDR, sizeof(NodeId_t));
+        retries = 0;
+
+        retries++;
+        Serial.print(F("req nxt neigh "));
+        Serial.println(retries);
+        Wire.clearWireTimeoutFlag();
+        Wire.requestFrom(FINDER_I2C_ADDR, sizeof(NodeId_t));
+        if (Wire.getWireTimeoutFlag())
+        {
+            delay(1000);
+        }
+    } while (Wire.getWireTimeoutFlag());
+
     while (Wire.available() < sizeof(NodeId_t))
     {
     }
@@ -85,12 +215,38 @@ NodeId_t PathFinder::getNextNeighborRequest()
 
 void PathFinder::resetIterator(NodeId_t start)
 {
-    Wire.beginTransmission(FINDER_I2C_ADDR);
-    Wire.write(FINDER_ITERATOR_RESET);
-    Wire.write((byte *)&start, sizeof(start));
-    Wire.endTransmission();
+    int retries = 0;
+    do
+    {
+        do
+        {
+            retries++;
+            Serial.print(F("trn rst iter "));
+            Serial.println(retries);
+            Wire.clearWireTimeoutFlag();
+            Wire.beginTransmission(FINDER_I2C_ADDR);
+            Wire.write(FINDER_ITERATOR_RESET);
+            Wire.write((byte *)&start, sizeof(start));
+            Wire.endTransmission();
+            if (Wire.getWireTimeoutFlag())
+            {
+                delay(1000);
+            }
+        } while (Wire.getWireTimeoutFlag());
 
-    Wire.requestFrom(FINDER_I2C_ADDR, 1);
+        retries = 0;
+
+        retries++;
+        Serial.print(F("req rst iter "));
+        Serial.println(retries);
+        Wire.clearWireTimeoutFlag();
+        Wire.requestFrom(FINDER_I2C_ADDR, 1);
+        if (Wire.getWireTimeoutFlag())
+        {
+            delay(1000);
+        }
+    } while (Wire.getWireTimeoutFlag());
+
     while (Wire.available() < 1)
     {
     }
@@ -98,11 +254,37 @@ void PathFinder::resetIterator(NodeId_t start)
 
 void clearIterator()
 {
-    Wire.beginTransmission(FINDER_I2C_ADDR);
-    Wire.write(FINDER_ITERATOR_CLEAR);
-    Wire.endTransmission();
+    int retries = 0;
+    do
+    {
+        do
+        {
+            retries++;
+            Serial.print(F("trn clear iter "));
+            Serial.println(retries);
+            Wire.clearWireTimeoutFlag();
+            Wire.beginTransmission(FINDER_I2C_ADDR);
+            Wire.write(FINDER_ITERATOR_CLEAR);
+            Wire.endTransmission();
+            if (Wire.getWireTimeoutFlag())
+            {
+                delay(1000);
+            }
+        } while (Wire.getWireTimeoutFlag());
 
-    Wire.requestFrom(FINDER_I2C_ADDR, 1);
+        retries = 0;
+
+        retries++;
+        Serial.print(F("req clear iter "));
+        Serial.println(retries);
+        Wire.clearWireTimeoutFlag();
+        Wire.requestFrom(FINDER_I2C_ADDR, 1);
+        if (Wire.getWireTimeoutFlag())
+        {
+            delay(1000);
+        }
+    } while (Wire.getWireTimeoutFlag());
+
     while (Wire.available() < 1)
     {
     }
@@ -110,11 +292,37 @@ void clearIterator()
 
 NodeId_t PathFinder::getIteratorNext()
 {
-    Wire.beginTransmission(FINDER_I2C_ADDR);
-    Wire.write(FINDER_ITERATOR_NEXT);
-    Wire.endTransmission();
+    int retries = 0;
+    do
+    {
+        do
+        {
+            retries++;
+            Serial.print(F("trn nxt iter "));
+            Serial.println(retries);
+            Wire.clearWireTimeoutFlag();
+            Wire.beginTransmission(FINDER_I2C_ADDR);
+            Wire.write(FINDER_ITERATOR_NEXT);
+            Wire.endTransmission();
+            if (Wire.getWireTimeoutFlag())
+            {
+                delay(1000);
+            }
+        } while (Wire.getWireTimeoutFlag());
 
-    Wire.requestFrom(FINDER_I2C_ADDR, sizeof(NodeId_t));
+        retries = 0;
+
+        retries++;
+        Serial.print(F("req nxt iter "));
+        Serial.println(retries);
+        Wire.clearWireTimeoutFlag();
+        Wire.requestFrom(FINDER_I2C_ADDR, sizeof(NodeId_t));
+        if (Wire.getWireTimeoutFlag())
+        {
+            delay(1000);
+        }
+    } while (Wire.getWireTimeoutFlag());
+
     while (Wire.available() < sizeof(NodeId_t))
     {
     }
@@ -125,11 +333,37 @@ NodeId_t PathFinder::getIteratorNext()
 
 void PathFinder::clearTopology()
 {
-    Wire.beginTransmission(FINDER_I2C_ADDR);
-    Wire.write(FINDER_CLEAR_TOPOLOGY);
-    Wire.endTransmission();
+    int retries = 0;
+    do
+    {
+        do
+        {
+            retries++;
+            Serial.print(F("trn clear topo "));
+            Serial.println(retries);
+            Wire.clearWireTimeoutFlag();
+            Wire.beginTransmission(FINDER_I2C_ADDR);
+            Wire.write(FINDER_CLEAR_TOPOLOGY);
+            Wire.endTransmission();
+            if (Wire.getWireTimeoutFlag())
+            {
+                delay(1000);
+            }
+        } while (Wire.getWireTimeoutFlag());
 
-    Wire.requestFrom(FINDER_I2C_ADDR, 1);
+        retries = 0;
+
+        retries++;
+        Serial.print(F("req clear topo "));
+        Serial.println(retries);
+        Wire.clearWireTimeoutFlag();
+        Wire.requestFrom(FINDER_I2C_ADDR, 1);
+        if (Wire.getWireTimeoutFlag())
+        {
+            delay(1000);
+        }
+    } while (Wire.getWireTimeoutFlag());
+
     while (Wire.available() < 1)
     {
     }
@@ -137,12 +371,38 @@ void PathFinder::clearTopology()
 
 void PathFinder::getAdjacent(NodeId_t node, Set<NodeId_t> &adjacent)
 {
-    Wire.beginTransmission(FINDER_I2C_ADDR);
-    Wire.write(FINDER_GET_ADJACENT);
-    Wire.write((byte *)&node, sizeof(node));
-    Wire.endTransmission();
+    int retries = 0;
+    do
+    {
+        do
+        {
+            retries++;
+            Serial.print(F("trn get adj "));
+            Serial.println(retries);
+            Wire.clearWireTimeoutFlag();
+            Wire.beginTransmission(FINDER_I2C_ADDR);
+            Wire.write(FINDER_GET_ADJACENT);
+            Wire.write((byte *)&node, sizeof(node));
+            Wire.endTransmission();
+            if (Wire.getWireTimeoutFlag())
+            {
+                delay(1000);
+            }
+        } while (Wire.getWireTimeoutFlag());
 
-    Wire.requestFrom(FINDER_I2C_ADDR, sizeof(NodeId_t) * maxNeighbors + sizeof(size_t));
+        retries = 0;
+
+        retries++;
+        Serial.print(F("req get adj "));
+        Serial.println(retries);
+        Wire.clearWireTimeoutFlag();
+        Wire.requestFrom(FINDER_I2C_ADDR, sizeof(NodeId_t) * maxNeighbors + sizeof(size_t));
+        if (Wire.getWireTimeoutFlag())
+        {
+            delay(1000);
+        }
+    } while (Wire.getWireTimeoutFlag());
+
     while (Wire.available() < sizeof(size_t))
     {
     }
